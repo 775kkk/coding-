8b481fc5cc4003bf0c6f0a4a2372d6728a344ab3
Nikita Slaanesh <filonenko77500@gmail.com>
2024-03-03 17:52:35 +0300

    mood ечейки

--- decisions_ege_2024/26/yacheikiHronenya/test_field.py
+++ decisions_ege_2024/26/yacheikiHronenya/test_field.py
@@ -3,7 +3,8 @@ k=int(f.readline())
 n=int(f.readline())
 a=sorted([list(map(int,i.split())) for i in f])
 a1=a
-fs=[]
+print(a[0])
+sw=[]
 c=0
 mx=0
 for j in range(k):
@@ -14,11 +15,20 @@ for j in range(k):
         if a[i][0]>t[-1][1]:
             t.append(a[i])
             l.append(i)
-            mx=max(mx,a[i][0])
+            if a[i][0]==a1[-1][0]:
+                print(j+1)
+            # mx=max(mx,a[i][0])
+            # if a[i][0]==998:
+            #     sw.append(j+1)
     for i in l:
         a.pop(i)
     c+=len(t)
-    fs.append(t)
-    print(j+1,t)
-print(c)
-print(mx)
+    #print(j+1,t)
+
+print(c)# ОТВЕТ В ПУНКТЕ А 344 (сока пассажиров сдало багаж)
+print(mx)# нашли время сдачи последнего багажа - 998
+#номер ячейки к пункту в - 31
+print(sw)
+print(a1[-1])
+#31 [998, 1338]
+#53 [998, 1338]
