944fe05c6a12db9b675d946a4041e0b2a34f8bc4
Nikita Slaanesh <filonenko77500@gmail.com>
2024-03-07 16:28:06 +0300

    pract3_6

---
+++ futureCode/pract3_6/helo.java
@@ -0,0 +1,33 @@
+package futureCode.pract3_6;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class helo {
+    private static String WhatISyourName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя :");
+        return scanner.nextLine();
+        
+    }
+    private static void writeToFile(String content, String fileName){
+        try(FileWriter writer = new FileWriter(fileName)){
+            writer.write(content);
+        }catch(IOException e){
+            System.err.println("Ошибка при записи в файл : "+ e.getMessage());
+        }
+    }
+    public static void haloUser(String userName){
+        System.out.println("halo!, "+userName+".");
+    }
+    public static void main(String[] args) {
+        String userName = WhatISyourName();
+        writeToFile(userName, "futureCode//pract3_6/userName.txt");
+        haloUser(userName);
+    }
+
+
+
+
+}
---
+++ futureCode/pract3_6/inputRead.java
@@ -0,0 +1,8 @@
+package futureCode.pract3_6;
+
+public class inputRead  extends textRead{
+    public static void main(String[] args) {
+        fileName = "futureCode/pract3_6/input.txt";
+        textRead.main(args);
+    }
+}
