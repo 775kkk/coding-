8e32c5bd10c65617ab3be7d07d964a81316f4c6b
Nikita Slaanesh <filonenko77500@gmail.com>
2024-03-08 22:50:31 +0300

    примечания

--- decisions_ege_2024/пробник от яндекса 08 03 24/16.py
+++ decisions_ege_2024/пробник от яндекса 08 03 24/16.py
@@ -2,6 +2,9 @@ from functools import lru_cache
 @lru_cache(None)
 def f(n):
     if n<=1:
-        return 1
+        return 1#сам исправил тут 0.5 на 1, тк в
+    #условии задачи сказано что n - натуральное
+    # а при подстановке оригинального кода выводилось nan, что
+    # значит "nan" (Not a Number) как результат деления очень больших чисел.
     return(n+1)*f(n-1)
 print(f(200)/f(198))
