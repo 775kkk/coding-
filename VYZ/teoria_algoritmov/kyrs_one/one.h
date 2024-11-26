e7fb2793c180753b48d4ce085856c11ecf33099e
775kkk <filonenko77500@gmail.com>
2024-11-26 23:36:35 +0300

    2 практики было но публикую ток ща тк на одной ебланил на второй пыхтел но получил автомат а пыхтел из за того что сишник язык низкоуровненвый какой же костыль этот ваш дефайн макрос для замены кранты просто это вроде нарушение всех морально этических норм программирования не?

--- VYZ/teoria_algoritmov/kyrs_one/one.h
+++ VYZ/teoria_algoritmov/kyrs_one/one.h
@@ -23,3 +23,48 @@ int* vstavka_v_mass( int arr[],int size, int poz, int znachenie){
     }
     return arr2;
 }
+
+char* vstavka_v_str(char* str,char* vstavka,int index){
+    char str[100] = "ababa";
+    char str2[] = "aba";
+    int count = 0;
+    for (int i = 0, j = 0; str[i]!='\0'; i++){
+        if (str[i] == str2[0]){
+            j = 0;
+            for (;str2[j]!='\0'; j++){
+                if (str[i+j] != str2[j]) break;
+            }
+        if (str2[j] == '\0') count++;
+        }
+    }
+}
+
+int revers(int a){
+    int f = 0;
+    while (a>0)
+    {
+        f= f*10+a%10;
+        a=a/10;
+    }
+    return f;
+    
+}
+int dvoi(int a){
+    int f = 1;
+    while (a>0)
+    {
+        f= f*10+a%2;    
+        a=a/2;
+    }
+    return revers(f)/10;
+}
+int dvoi1(int a){
+	int res = 0;
+	int toW = 1;
+	while(a>0){
+		res += (a&1)*toW;
+		toW*=10;
+		a=a>>1;
+	}
+	return res;
+}
---
+++ VYZ/teoria_algoritmov/kyrs_one/zan-19-11-24/one.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "../one.h"
+
+int main(){
+    // char str[30] ="aBcD";
+	// int len = 0;
+	// for(;str[len]!='\0';len++);
+	// printf("%i\n", A-a);
+	// puts(str);//a=97 A=65
+	// str[0]-=32;
+	// str[1]+=32;
+	// str[2]-=32;
+	// str[3]+=32;
+//	printf("%i %i",str[0] ,str[1]);
+	// puts(str);
+
+    // найти колво вхождений "ab"
+   чек оне точка аш
+}
