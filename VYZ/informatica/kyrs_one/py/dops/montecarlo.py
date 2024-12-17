5ec138d7503cd5918518d9cfc24b7079f4429edd
Nikita Slaanesh <filonenko77500@gmail.com>
2024-12-17 17:32:33 +0300

    метод монте карло аплоад

--- VYZ/informatica/kyrs_one/py/dops/montecarlo.py
+++ VYZ/informatica/kyrs_one/py/dops/montecarlo.py
@@ -3,12 +3,12 @@ import random
 import math
 
 def calculate_pi(n):
-    window = gr.GraphWin("Monte Carlo", 500, 500)
+    window = gr.GraphWin("пи кружок в квадратике(окне)", 550, 550)
     square = gr.Rectangle(gr.Point(0, 0), gr.Point(500, 500))
-    square.draw(window)
     circle = gr.Circle(gr.Point(250, 250), 250)
+    square.draw(window)
     circle.draw(window)
-    points_inside_circle = 0
+    pozInKryg = 0
     results = [] 
 
     for i in range(1, n + 1):
@@ -18,22 +18,16 @@ def calculate_pi(n):
         point.draw(window)
 
         if (x - 250)*2 + (y - 250)*2 <= 250*2:
-            points_inside_circle += 1
+            pozInKryg += 1
             point.setFill('blue')
         else:
             point.setFill('red')
 
         if i % 100 == 0: 
-            pi_approx = 4 * points_inside_circle / i
-            relative_error = abs((math.pi - pi_approx) / math.pi) *100
-            results.append((i, relative_error))
-            print(f"Points: {i}, Approximation of π: {pi_approx}, Relative Error: {relative_error:.4f}%")
-
-
-    with open('result.csv', 'w') as f:
-        f.write("Points,Relative Error\n")
-        for points, error in results:
-            f.write(f"{points},{error}\n")
+            onlinePI = 4 * pozInKryg / i
+            otnosErorOfPI = abs((math.pi - onlinePI) / math.pi) *100
+            results.append((i, otnosErorOfPI))
+            print(f"Точек нанесено: {i}, Онлайн число π: {onlinePI}, Относительная ошибка: {otnosErorOfPI:.4f}%")
 
     window.getMouse()
     window.close()
