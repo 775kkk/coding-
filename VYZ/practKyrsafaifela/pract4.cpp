a7f8f701b3a17597ac6c0ec2f6dfbbbaedafd9fe
Nikita <filonenko77500@gmail.com>
2025-02-16 02:26:12 +0400

    Добавлены функции для отрисовки графиков и вычисления значений, оптимизирована логика работы с массивами. кароче там еще задачка на площадь под графиками и площадь меж двумя графиками но я ваще не особо врубаюсь как буду вычислять область в которой мне надо будет считать подчеркивания, особенно у параболы у которой только точки))) интегралы придумали не просто так чтобы я их ща не вынужден был использовать, кароч ди нах задача

--- VYZ/practKyrsafaifela/pract4.cpp
+++ VYZ/practKyrsafaifela/pract4.cpp
@@ -2,49 +2,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
-//надо отрисовать график плоскости с ограничениями ну по иксу и игрику
-// char** grafik(int plusX, int minusX, int plusY, int minusY, int k, int b){
-//     int lenOneStr = ((plusX<0 ? +(plusX*-1) : +(plusX))+(minusX<0 ? +(minusX*-1) : +(minusX)))+1;//+1 на херню за ноль, за '\0' учтем вконце
-//     int lenAllStr = ((plusY<0 ? +(plusY*-1) : +(plusY))+(minusY<0 ? +(minusY*-1) : +(minusY)))+1;//тупа по модулю все суммируем
-//     char** ITOGgrafik = (char**)malloc((lenAllStr)*sizeof(char*));
-//     for (int i = 0; i < lenAllStr; i++)
-//     {   
-//         //отрисовка строки
-//         ITOGgrafik[i]=(char*)malloc((lenOneStr+1)*sizeof(char));
-//         // if (i==(lenAllStr-1)/2) - черты(пересечение нулей) -> не забудь ноль ноль
-//         // if (j==lenOneStr/2) - пересечение нуля в строке
-//         //  ITOGgrafik[i][lenOneStr+1]='\0';
-//         // уравнение отрисовки ==> (lenAllStr-i)==(k*j)+b
-//         //  Y=kx+b
-
-//         if (i==(lenAllStr-1)/2)
-//         {
-//             // ITOGgrafik[i]=(char*)malloc((lenOneStr+1)*sizeof(char));
-//             *ITOGgrafik[i]=('-'*minusX)+('+')+('-'*plusX)+'\0'; // ТУТ ОШИБК ТИПА МЫ ВАЩЕ В ЛЮБОМ СЛУАЧАЕ ОТРИСУЕМ ПЕРЕСЕЧЕНИЕ БЕЗ ВСТАВКИ ПРЕДПОЛАГАЕМОЙ ЗВЕЗДЫ??!!
-//         }
-//         else{
-//             for (int j = 0; j < lenOneStr; j++)
-//             {
-//                 //определяемся
-//                 if (j==lenOneStr/2)
-//                 {
-//                     ITOGgrafik[i][j]='|';
-//                 }
-//                 else
-//                 {
-//                     // тут  уже проверяем пусто или звездочка
-//                     if (lenAllStr-i==)//формула по которой узнаем звезда ли
-//                     {
-//                         /* code */
-//                     }else{ ITOGgrafik[i][j]='_';}
-                    
-//                 }
-//             }
-           
-//         }
-//     }
-// }
-
+//я хер знает как организовать данные
 char** grafik(int plusX, int minusX, int plusY, int minusY){
     int lenOneStr = ((plusX<0 ? +(plusX*-1) : +(plusX))+(minusX<0 ? +(minusX*-1) : +(minusX)))+1;//+1 на херню за ноль, за '\0' учтем вконце
     int lenAllStr = ((plusY<0 ? +(plusY*-1) : +(plusY))+(minusY<0 ? +(minusY*-1) : +(minusY)))+1;//тупа по модулю все суммируем
@@ -52,56 +10,90 @@ char** grafik(int plusX, int minusX, int plusY, int minusY){
     for (int i = 0; i < lenAllStr; i++)
     {   
         //отрисовка строки
-        ITOGgrafik[i]=(char*)malloc((lenOneStr+1)*sizeof(char));
-        if (i==(lenAllStr-1)/2)
-        {
-            // ITOGgrafik[i]=(char*)malloc((lenOneStr+1)*sizeof(char));
-            // ITOGgrafik[i]=('-'*minusX)+('+')+('-'*plusX)+'\0';
-            for (int j = 0; j < lenOneStr; i++)
-            {
-                //определяемся
-                if (j==lenOneStr/2)
-                {
-                    ITOGgrafik[i][j]='+';
-                }
-                else
-                {
-                    ITOGgrafik[i][j]='-';
-                }
-            }
-        }
-        else
-        {
+        (ITOGgrafik)[i]=(char*)malloc((lenOneStr+1)*sizeof(char));
             for (int j = 0; j < lenOneStr; j++)
             {
-                //определяемся
-                if (j==lenOneStr/2)
+                if (j==(minusX<0 ? (minusX*-1) : (minusX)))
                 {
-                    ITOGgrafik[i][j]='|';
-                }
-                else
-                {
-                    ITOGgrafik[i][j]='_';
+                    if (i==plusY){
+                        (ITOGgrafik)[i][j]='+';
+                    }
+                    else{
+                        (ITOGgrafik)[i][j]='|';
+                    }
+                }else if (i==plusY){
+                    (ITOGgrafik)[i][j]='=';
+                }else{
+                    (ITOGgrafik)[i][j]='-';
                 }                
-            } 
-            // puts(ITOGgrafik[i]);
-        }       
-        ITOGgrafik[i][lenOneStr+1]='\0';  
-        puts(ITOGgrafik[i]);
+            }    
+        (ITOGgrafik)[i][lenOneStr]='\0';  
+        // puts((*ITOGgrafik)[i]);
     }
-    return ITOGgrafik;
+     return ITOGgrafik;
+}
+
+void kxbFunc(char** grafik,int plusX, int minusX, int plusY, int minusY, int k, int b){
+    int lenOneStr = ((plusX<0 ? +(plusX*-1) : +(plusX))+(minusX<0 ? +(minusX*-1) : +(minusX)))+1;//+1 на херню за ноль, за '\0' учтем вконце
+    int lenAllStr = ((plusY<0 ? +(plusY*-1) : +(plusY))+(minusY<0 ? +(minusY*-1) : +(minusY)))+1;//тупа по модулю все суммируем
+    int mainY;
+    int mainX;
+    for (int i = 0; i < lenAllStr; i++)
+    {
+        mainY=(plusY<0 ? +(plusY*-1) : +(plusY))-i;
+        for (int j = 0; j < lenOneStr; j++)
+        {
+            mainX=(minusX<0 ? (minusX) : (minusX*-1))+j;
+            if (mainY==(k*mainX)+b)
+            {
+                grafik[i][j]='*';
+                // printf("i=%d j=%d\n",i,j);
+                // printf("lenAllStr-1-i=%d (k*j)-b=%d\n",lenAllStr-1-i,(k*j)-b);
+            }
+        }
+        // puts(grafik[i]);
+    }
+}
+
+void parabolaFunc(char** grafik,int plusX, int minusX, int plusY, int minusY, int a, int b, int c){
+    int lenOneStr = ((plusX<0 ? +(plusX*-1) : +(plusX))+(minusX<0 ? +(minusX*-1) : +(minusX)))+1;//+1 на херню за ноль, за '\0' учтем вконце
+    int lenAllStr = ((plusY<0 ? +(plusY*-1) : +(plusY))+(minusY<0 ? +(minusY*-1) : +(minusY)))+1;//тупа по модулю все суммируем
+    int mainY;
+    int mainX;
+    for (int i = 0; i < lenAllStr; i++)
+    {
+        mainY=(plusY<0 ? +(plusY*-1) : +(plusY))-i;
+        for (int j = 0; j < lenOneStr; j++)
+        {
+            mainX=(minusX<0 ? (minusX) : (minusX*-1))+j;
+            if (mainY==(a*mainX*mainX)+(b*mainX)+c)
+            {
+                grafik[i][j]='*';
+                // printf("i=%d j=%d\n",i,j);
+                // printf("lenAllStr-1-i=%d (k*j)-b=%d\n",lenAllStr-1-i,(k*j)-b);
+            }
+        }
+        // puts(grafik[i]);
+    }
 }
 
 int main(){
-
-    char** gr = grafik(5,5,5,5);
-    for (int i = 0; i < 11; i++)
+    int plusX=20;
+    int minusX=20;
+    int plusY=25; 
+    int minusY=20;
+    // char** gr = grafik(5,5,5,5);
+    char** gr = grafik(plusX,minusX,plusY,minusY);
+    kxbFunc(gr,plusX,minusX,plusY,minusY,1,0);
+    parabolaFunc(gr,plusX,minusX,plusY,minusY,1,0,0);
+    // kxbFunc(gr,sizeDataPack[0],sizeDataPack[1],1,0);
+    // kxbFunc(gr,sizeDataPack[0],sizeDataPack[1],1,1);
+    // parabolaFunc(gr,sizeDataPack[0],sizeDataPack[1],1,0,0);
+    int lenAllStr = ((plusY<0 ? +(plusY*-1) : +(plusY))+(minusY<0 ? +(minusY*-1) : +(minusY)))+1;
+    for (int i = 0; i <lenAllStr; i++)
     {   
-        for (int j = 0; gr[j]!='\0'; i++)
-        {
-            printf("%s",gr[i][j]);
-        }
+        puts(gr[i]);
     }
-    
-
+    // кароче там еще задание посчитать площадь под графиком и площадь меж двумя графиками(параболами) но я рт е прост бы формулой какойто посчитал и мозги не делал
+    // под линейным это то еще ладно, под параболой тоже аналогично мб хотя там не везде звездочки, а вот меж двумя параболами это капецы
 }
