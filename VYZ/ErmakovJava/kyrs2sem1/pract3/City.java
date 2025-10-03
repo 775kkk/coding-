c0fc35feddcc47a3a31f2fe65f9e88ed85fb8e96
Nikita <filonenko77500@gmail.com>
2025-10-03 10:31:21 +0400

    Экстренный утренний попытка доделать практику(неуспешная- не полная - думаю валидная)

---
+++ VYZ/ErmakovJava/kyrs2sem1/pract3/City.java
@@ -0,0 +1,12 @@
+package VYZ.ErmakovJava.kyrs2sem1.pract3;
+
+import java.util.List;
+
+public class City {
+    private String cityName;
+    private List<Road> roadList;
+    // 
+    // конструктор мейн, конструктор без имени - номер от статики(id)
+    // ,конструктор копии, конструктор с аргументами типа Road, 
+    // см далее примеры построения класса роад полилейн точки студента
+}
---
+++ VYZ/ErmakovJava/kyrs2sem1/pract3/Road.java
@@ -0,0 +1,65 @@
+package VYZ.ErmakovJava.kyrs2sem1.pract3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import futureCode.blok_3.pract3_7.forcalco;
+
+public class Road {
+    private String roadName;// дефолт роаднейма будет соедененное название двух городов
+    private  List<City> linkedCitiesList;
+
+    private String roadNameCounted(String roadName){
+        if (roadName==null){
+            if (this.linkedCitiesList.size() >=2) {
+                return 
+                this.linkedCitiesList.get(0).getCityName()+" - "
+                +this.linkedCitiesList.get(1).getCityName();
+            }
+            if (this.linkedCitiesList.size()==1) {
+                return this.linkedCitiesList.get(0).getCityName();
+            }
+        }
+        return roadName;
+        
+    }
+    public Road(String roadName, List<City> linkedCitiesList){
+        this.linkedCitiesList = new ArrayList<City>(linkedCitiesList);
+        this.roadName = roadNameCounted(roadName);
+        
+    }
+    public Road(List<City> linkedCitiesList){
+        this(null, linkedCitiesList);
+    }
+    public Road(City... argsOfCities) {
+        this(null, Arrays.asList(argsOfCities));
+    }
+
+    public void setLinkedCities(List<City> linkedCitiesList) {
+        this.linkedCitiesList = new ArrayList<City>(linkedCitiesList);
+    }
+    public void setRoadName(String roadName) {
+        this.roadName = roadName;
+    }
+
+    public List<City> getLinkedCitiesList() {
+        return new ArrayList<City>(linkedCitiesList);
+    }
+    public String getRoadName() {
+        return roadName;
+    }
+
+    public void addCities(City city){
+        this.linkedCitiesList.add(new City(city));
+    }
+    public void addCities(City... argsOfCities){
+        for (City city : argsOfCities) {
+            this.addCities(city);
+        }
+    }
+
+    // гет по индексу, ремув по индексу, поп, тустринг
+    
+
+}
