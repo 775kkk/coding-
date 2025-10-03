233243993c9695b90210dc45187e54f2f0c15d24
775kkk <filonenko77500@gmail.com>
2025-10-03 12:52:03 +0400

    доделаем после обеда

--- VYZ/ErmakovJava/kyrs2sem1/pract3/Road.java
+++ VYZ/ErmakovJava/kyrs2sem1/pract3/Road.java
@@ -1,14 +1,16 @@
--package VYZ.ErmakovJava.kyrs2sem1.pract3;
+package VYZ.ErmakovJava.kyrs2sem1.pract3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import futureCode.blok_3.pract3_7.forcalco;
-
 public class Road {
     private String roadName;// дефолт роаднейма будет соедененное название двух городов
-    private  List<City> linkedCitiesList;
+    private List<City> linkedCitiesList;// может список сделать отдельной сучностью и у него поведения определить?????????
+    // может список сделать отдельной сучностью и у него поведения определить?????????
+    // может список сделать отдельной сучностью и у него поведения определить?????????
+    // может список сделать отдельной сучностью и у него поведения определить?????????
+    private int costRoute;
 
     private String roadNameCounted(String roadName){
         if (roadName==null){
@@ -21,19 +23,23 @@ public class Road {
                 return this.linkedCitiesList.get(0).getCityName();
             }
         }
-        return roadName;
-        
+        return roadName;   
     }
-    public Road(String roadName, List<City> linkedCitiesList){
+    
+    public Road(String roadName,  int costRoute,List<City> linkedCitiesList){
         this.linkedCitiesList = new ArrayList<City>(linkedCitiesList);
         this.roadName = roadNameCounted(roadName);
+        this.costRoute = costRoute;
         
     }
-    public Road(List<City> linkedCitiesList){
-        this(null, linkedCitiesList);
+    public Road(List<City> linkedCitiesList, int costRoute){
+        this(null, costRoute, linkedCitiesList);
     }
-    public Road(City... argsOfCities) {
-        this(null, Arrays.asList(argsOfCities));
+    public Road(int costRoute,City... argsOfCities) {
+        this(null, costRoute, Arrays.asList(argsOfCities));
+    }
+    public Road(Road road){
+        this(road.getRoadName(),road.getCostRoute(),road.getLinkedCitiesList());
     }
 
     public void setLinkedCities(List<City> linkedCitiesList) {
@@ -42,6 +48,9 @@ public class Road {
     public void setRoadName(String roadName) {
         this.roadName = roadName;
     }
+    public void setCostRoute(int costRoute) {
+        this.costRoute = costRoute;
+    }
 
     public List<City> getLinkedCitiesList() {
         return new ArrayList<City>(linkedCitiesList);
@@ -49,27 +58,41 @@ public class Road {
     public String getRoadName() {
         return roadName;
     }
+    public int getCostRoute() {
+        return costRoute;
+    }
 
-    public void addCities(City city){
+    public void addCity(City city){
+        city.addNewRoadtoRoadList(this);// вызываем метод сити где он Добавляет новую дорогу в список дорог
+        this.addCityToCityList(city);//вызываем метод дороги для записи города в список дороги.
+    }
+
+    public void addCityToCityList(City city){
         this.linkedCitiesList.add(new City(city));
     }
-    public void addCities(City... argsOfCities){
+    public void addCityToCityList(City... argsOfCities){
         for (City city : argsOfCities) {
-            this.addCities(city);
+            this.addCityToCityList(city);
         }
     }
 
     public City getCityOnIndex(int index){// НЕ КОПИЯ
         return this.linkedCitiesList.get(index);
     }
-    public void popCityInRoadList(){
+    public void popCityFromCityList(){
+        this.linkedCitiesList.get(this.linkedCitiesList.size()-1).removeRoadFromRoadListByObject(this);
         this.linkedCitiesList.remove(this.linkedCitiesList.size()-1);
+
     }
-    public void removeCityInRoadListByIndex(int index){
+    public void removeCityFromCityListByIndex(int index){
+        (this.linkedCitiesList.get(index)).removeRoadFromRoadListByObject(this);
         this.linkedCitiesList.remove(index);
     }
+    public void removeCityFromCityListByObject(City city){
+        this.linkedCitiesList.remove(city);
+    }
 
-    public String getCitiesNamesInListToString(){
+    public String getCityNamesFromCityListToString(){
         if (this.linkedCitiesList.isEmpty()) {
             return "No linked Cities";
         }
@@ -82,6 +105,6 @@ public class Road {
 
     @Override
     public String toString() {
-        return "Road "+this.roadName+"; Cities:"+getCitiesNamesInListToString();
+        return "Road "+this.roadName+"; Cities:"+getCityNamesFromCityListToString();
     }
 }
