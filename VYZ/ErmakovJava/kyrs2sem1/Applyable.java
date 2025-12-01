5147045b1f18af146ca3e379997a711e43899449
775kkk <filonenko77500@gmail.com>
2025-12-01 15:11:57 +0400

    позорнейшие джейнерики(говнореики) просто фу бе и задания фу бе чтото о чемто ни про что бл 6.3 end

---
+++ VYZ/ErmakovJava/kyrs2sem1/Applyable.java
@@ -0,0 +1,5 @@
+package VYZ.ErmakovJava.kyrs2sem1;
+
+public interface Applyable<T,P> {
+    public P Apply(T t);   
+}
---
+++ VYZ/ErmakovJava/kyrs2sem1/Testyable.java
@@ -0,0 +1,5 @@
+package VYZ.ErmakovJava.kyrs2sem1;
+
+public interface Testyable<T> {
+    public boolean test(T t);
+}
